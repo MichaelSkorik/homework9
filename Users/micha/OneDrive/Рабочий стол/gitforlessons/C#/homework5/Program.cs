a00f6a8c5cfbbb489dc1c6d65cf7a0b69cd02236
Michael <Michael@SKorik.com>
2023-07-19 14:44:31 +0300

    Ex_38

--- Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework5/Program.cs
+++ Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework5/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿Console.Clear();
+int arraySize = new Random().Next(2, 8);
+double[] array = new double[arraySize];
+int size = 0;
+for (int i = 0; i < arraySize; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+// Вызываем метод, который найдет разницу между максимальным и минимальным элементами массива
+double difference = FindDifference(array);
+
+// Выводим результат на консоль
+var str = string.Join(" ", array);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива [{str}] --> {difference}");
+
+double FindDifference(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        else if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+
+    // Возвращаем разницу между максимальным и минимальным элементами
+    return max - min;
+}
+
