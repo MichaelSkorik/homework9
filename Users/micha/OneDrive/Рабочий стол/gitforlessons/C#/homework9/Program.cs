c55f752fe1d39389b61e9c91bc88bdac3e5895da
Michael <Michael@SKorik.com>
2023-08-04 13:32:30 +0300

    Ex_64

--- Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework9/Program.cs
+++ Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework9/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int GetFactorial(int N)
+{
+    if (N == 1)
+        return 1;
+    
+    return N * GetFactorial(N - 1);
+}
+
+int Print(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = Print("Enter number");
+int factorial = GetFactorial(N);
+Console.WriteLine($"Factorial of {N} is: {factorial}");
