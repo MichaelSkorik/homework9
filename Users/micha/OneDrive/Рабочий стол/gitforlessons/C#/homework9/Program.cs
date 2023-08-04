2400bfe6b420e90cebefefb1fa839314ded081fe
Michael <Michael@SKorik.com>
2023-08-04 14:12:09 +0300

    Ex_66

--- Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework9/Program.cs
+++ Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework9/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int GetSum(int N, int M)
+{
+    if (N == M)
+    {
+        return M;
+    }
+    else
+    {
+    return N + GetSum(N - 1, M);
+    }
+}
+
+int Print(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = Print("Введите число N");
+int M = Print("Введите число M");
+int sum = GetSum(N, M);
+Console.Write($"Сумма чисел от {N} до {M} -> {sum}");
+
